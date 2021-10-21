4913ebaeeaf83e0c6100938bd572ab6447f5c775
Tae Hee Choi <dadac76@hotmail.com>
2021-10-20 21:59:26 -0700

    first commit

---
+++ demo.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from typing import List
+
+import pysftp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def file_list() -> List[str]:
+    return [str(path) for path in Path().glob('data/*.*')] # path for all files in data folder
+
+def main():
+    sftp_host = os.getenv("SFTP_IPADDRESS")
+    sftp_userid = os.getenv("SFTP_USERID")
+    sftp_private_key = os.getenv("SFTP_PRIVATE_KEY")
+
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+   
+    try:
+        with pysftp.Connection(host=sftp_host, username=sftp_userid, private_key=sftp_private_key, port=22, cnopts=cnopts) as sftp:
+            with sftp.cd('sftp'):
+                for file in file_list():
+                    sftp.put(file)
+                print('Files have been uploaded to sftp successfully.')
+    except:
+        print('Upload has failed.')
+
+if __name__ == '__main__':
+    main()
