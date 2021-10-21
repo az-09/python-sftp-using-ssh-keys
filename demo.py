4051195833c97f71dc2768e0e50bfdd4ef49579a
Tae Hee Choi <dadac76@hotmail.com>
2021-10-20 22:42:31 -0700

    removed cnopts

--- demo.py
+++ demo.py
@@ -14,13 +14,10 @@ def main():
     sftp_host = os.getenv("SFTP_IPADDRESS")
     sftp_userid = os.getenv("SFTP_USERID")
     sftp_private_key = os.getenv("SFTP_PRIVATE_KEY")
-
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None
    
     try:
-        with pysftp.Connection(host=sftp_host, username=sftp_userid, private_key=sftp_private_key, port=22, cnopts=cnopts) as sftp:
-            with sftp.cd('sftp'):
+        with pysftp.Connection(host=sftp_host, username=sftp_userid, private_key=sftp_private_key, port=22) as sftp:
+            with sftp.cd('sftp'): # target folder
                 for file in file_list():
                     sftp.put(file)
                 print('Files have been uploaded to sftp successfully.')
